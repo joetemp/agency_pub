5389539b89aba982942b411e173e68bc123b5cd7
Joe Templeton <joe.k.templeton@gmail.com>
2015-05-06 14:27:40 -0600

    Set up project files and added an empty nav. This is more or less to make sure everything is working.

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link rel="stylesheet" type="text/css" href="style/stylesheets/screen.css">        
+        <title>Web dev agency</title>
+    </head>
+   
+    <body>
+        <nav></nav>
+    <body>
+</html>
 
