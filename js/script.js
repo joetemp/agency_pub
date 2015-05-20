0e52d2fcf003b650255fb6dc34b5be18e195889f
Joe Templeton <joe.k.templeton@gmail.com>
2015-05-19 22:22:32 -0600

    Got Hammer.js working in a test capacity.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 $( document ).ready(function() {
     console.log("I'm ready!");
 
-    $( ".test" ).on( "click", function() {
+    var test = $( ".test" );
+    var ui = $( ".ui" );
+
+    test.on( "click", function() {
         console.log("Test has been clicked");
-        $( ".ui" ).addClass( "out" );
+        ui.addClass( "out" );
     });
+
+    var fuck = document.getElementById('fuck');
+
+    var mc = new Hammer(fuck);
+
+    mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+
+    mc.on("panleft panright panup pandown tap press", function(ev) {
+        fuck.textContent = ev.type +" gesture detected.";
+        console.log("stop touching me!"); 
+    });
+
 });
