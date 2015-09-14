ba7ac84751f1486a2d7ae6f31e16df5d2586b9ca
Joe Templeton <joe.k.templeton@gmail.com>
2015-09-13 20:14:02 -0600

    Accomplished more or less what I want... having a parallel svg that exists in the same physical space but is just separate. However... making the viewbox bigger makes the image smaller? I think it's a perspective thing.

--- js/buttons.js
+++ js/buttons.js
@@ -3,5 +3,5 @@ console.log("You'll see this if buttons is in theory working.");
 var s1 = Snap("#buttons");
 
 var waldo = s1.rect(0, 0, 10, 10, 3, 3).attr({
-    fill: "blue"
+    fill: "yellow"
 });
