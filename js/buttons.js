1a0836aa02b1faa073eedabacae7b4f1cdb8eb93
Joe Templeton <joe.k.templeton@gmail.com>
2015-09-13 21:36:56 -0600

    Added vol down button.

--- js/buttons.js
+++ js/buttons.js
@@ -2,6 +2,10 @@ console.log("You'll see this if buttons is in theory working.");
 
 var s1 = Snap("#buttons");
 
-var waldo = s1.rect(-0.015, 43.2, 1.2, 15.5, 1, 1).attr({
+var volUpButton = s1.rect(-0.015, 43.2, 1.2, 15.5, 1, 1).attr({
+    fill: "red"
+});
+
+var volDownButton = s1.rect(-0.015, 62.2, 1.2, 15.5, 1, 1).attr({
     fill: "red"
 });
