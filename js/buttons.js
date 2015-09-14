aeaf17ebd5df9e1df7dc05207e81e08b114fd4e2
Joe Templeton <joe.k.templeton@gmail.com>
2015-09-13 20:43:29 -0600

    Red vol up button added as proof of concept. Looks pretty damn good.

--- js/buttons.js
+++ js/buttons.js
@@ -2,6 +2,6 @@ console.log("You'll see this if buttons is in theory working.");
 
 var s1 = Snap("#buttons");
 
-var waldo = s1.rect(0, 0, 10, 10, 3, 3).attr({
-    fill: "yellow"
+var waldo = s1.rect(0, 44.2, 1, 14.4, 1, 1).attr({
+    fill: "red"
 });
