519302dbd2f49441399779bc2766c7eed2ec316c
Joe Templeton <joe.k.templeton@gmail.com>
2015-09-13 21:25:02 -0600

    Red mockup button has been improved and looks better. Use this example to finish the rest.

--- js/buttons.js
+++ js/buttons.js
@@ -2,6 +2,6 @@ console.log("You'll see this if buttons is in theory working.");
 
 var s1 = Snap("#buttons");
 
-var waldo = s1.rect(-0.015, 43.3, 1, 15.5, 1, 1).attr({
+var waldo = s1.rect(-0.015, 43.2, 1.2, 15.5, 1, 1).attr({
     fill: "red"
 });
